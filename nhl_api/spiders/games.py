455ccf9b2bbade7273f5f41e5bc38abd53ea9d8e
Paweł Sacawa <psacawa@math.toronto.edu>
2020-11-26 09:49:52 -0500

    Add: Scrape of all seasons

--- nhl_api/spiders/games.py
+++ nhl_api/spiders/games.py
@@ -2,6 +2,7 @@ import scrapy
 from scrapy.http import TextResponse, Request, Response
 import jq
 import json
+import requests
 
 class TeamsSpider(scrapy.Spider):
     name = 'teams'
@@ -26,62 +27,35 @@ class TeamsSpider(scrapy.Spider):
 class GamesSpider(scrapy.Spider):
     name = 'games'
     domain = 'statsapi.web.nhl.com'
-    api_root = 'https://{domain}/api/v1'
-    # contains an upper bound for the number of games in a season+gameType
-    #  as discovered at a certain point of a progressive crawl
-    games_limit = {}
+    api_root = f'https://{domain}/api/v1'
 
     def __init__(self, season=None, *args, **kwargs):
-        self.season=season
+        if season:
+            self.season_id=f"{season}{int(season)+1}"
         super().__init__(*args, **kwargs)
 
     def start_requests(self):
-        if self.season:
-            yield from self.crawl_season(self.season)
+        if hasattr(self, "season_id"):
+            yield from self.crawl_season(self.season_id)
         else:
             yield Request(url=f'{self.api_root}/seasons', callback=self.parse_seasons)
 
     def parse_seasons(self, response: TextResponse):
         json_response = json.loads(response.text)
-        seasons = jq.compile('.seasons[].seasonId[:4]').input(json_response).all()
-        for season in seasons:
-            yield from self.crawl_season(season)
+        seasons = jq.compile('.seasons[].seasonId').input(json_response).all()
+        for season_id in seasons:
+            yield from self.crawl_season(season_id)
 
-    def crawl_season(self, season: str):
-        yield from self.crawl_preseason(season)
-        yield from self.crawl_regular_season(season)
-        yield from self.crawl_playoffs(season)
+    def crawl_season(self, season_id: str):
+        print(f"crawl season {season_id}")
+        yield Request(url=f"{self.api_root}/schedule?season={season_id}",
+                callback=self.parse_schedule)
 
-    def crawl_preseason(self, season: str):
-        yield from self.crawl_progressive(f"{season}01", 150)
-
-    def crawl_regular_season(self, season: str):
-        yield from self.crawl_progressive(f"{season}02", 1200)
-
-    def crawl_progressive(self, key: str, limit: int):
-        """
-        For pre and regular seasons, when the # of games is uncertain, we have a
-        progressive crawl, which crawls with increasing indice, and stops if the indice
-        reaches the upper limit. This upper limit is hardcoded from above, and can
-        decrease when a crawl at a certain indice 404s.
-        """
-        self.games_limit[key] = limit
-        for i in range(limit):
-            game_id = f"{key}{i:04}"
-            if i >= self.games_limit[key]:
-                break
-            yield Request(url=f"{self.api_root}/game/{game_id}/feed/live",
-                    callback=self.parse_progressive, cb_kwargs={"key": key, "idx": i})
-
-    def parse_progressive(self, response: TextResponse, key, idx):
-        if response.status != 200:
-            self.games_limit['key'] = min (self.games_limit['key'], idx)
-        else:
-            yield json.loads(response.text)
-               
-
-    def crawl_playoffs(self, season: str):
-        
+    def parse_schedule(self, response: TextResponse):
+        schedule = json.loads(response.text)
+        game_links = jq.compile (".dates[].games[].link").input(schedule).all()
+        for link in game_links:
+            yield Request(url=f"https://{self.domain}{link}")
 
     def parse(self, response: TextResponse):
         if response.status == 200:
